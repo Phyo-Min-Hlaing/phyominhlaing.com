e9b587d79069cf7d609bc94df6e179f6cb9ad72f
Phyo Min Hlaing <phyo1401@gmail.com>
2026-07-23 10:32:37 +0630

    update home page

--- app/page.tsx
+++ app/page.tsx
@@ -3,17 +3,7 @@ import { Button } from "@/components/ui/button"
 export default function Page() {
   return (
     <div className="flex min-h-svh p-6">
-      <div className="flex max-w-md min-w-0 flex-col gap-4 text-sm leading-loose">
-        <div>
-          <h1 className="font-medium">Project ready!</h1>
-          <p>You may now add components and start building.</p>
-          <p>We&apos;ve already added the button component for you.</p>
-          <Button className="mt-2">Button</Button>
-        </div>
-        <div className="font-mono text-xs text-muted-foreground">
-          (Press <kbd>d</kbd> to toggle dark mode)
-        </div>
-      </div>
+      Hello, I'm Phyo Min Hlaing - Senior UI/UX and Product Designer. I have 10+ years of experience in designing and developing user interfaces for web and mobile applications. I specialize in creating intuitive and visually appealing designs that enhance user experience. My expertise includes wireframing, prototyping, user research, and visual design. I am passionate about creating products that solve real-world problems and delight users.
     </div>
   )
 }
